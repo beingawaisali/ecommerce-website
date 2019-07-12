640af1d490b2a7e65397c0e42946228e980701dc
Awais Ali <48981710+awais-malik716@users.noreply.github.com>
2019-07-12 17:21:33 +0500

    homepage-done

---
+++ script.js
@@ -0,0 +1,15 @@
+function showList(){
+    document.getElementById("menu-list").style.visibility = "visible";
+}
+
+function hideList(){
+    document.getElementById("menu-list").style.visibility = "hidden";
+}
+
+// function showSearch(){
+//     // document.getElementById("search-bar-mobile").style.width = "200px";
+//     document.getElementById("menu-bar").style.display = "none";
+//     document.getElementById("logo").style.display = "none";
+//     document.getElementById("cart").style.display = "none";
+
+// }
