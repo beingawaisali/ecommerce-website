cccd1ca2a7a71118668a1ec5c0c404b842d50589
Awais Ali <48981710+awais-malik716@users.noreply.github.com>
2019-07-12 17:29:58 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ function showList(){
 function hideList(){
     document.getElementById("menu-list").style.visibility = "hidden";
 }
-
-// function showSearch(){
-//     // document.getElementById("search-bar-mobile").style.width = "200px";
-//     document.getElementById("menu-bar").style.display = "none";
-//     document.getElementById("logo").style.display = "none";
-//     document.getElementById("cart").style.display = "none";
-
-// }
